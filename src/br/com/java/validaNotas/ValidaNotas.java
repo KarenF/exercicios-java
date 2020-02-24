ee666b7bfbf855e4db5122eb1d7d8070feee3366
KarenF <karenf@outlook.com>
2020-02-24 13:36:40 -0300

    add validaNotas

--- src/br/com/java/validaNotas/ValidaNotas.java
+++ src/br/com/java/validaNotas/ValidaNotas.java
@@ -2,6 +2,12 @@ package br.com.java.validaNotas;
 
 import java.util.Scanner;
 
+/*
+Faça um programa que peça uma nota, entre zero e dez. Mostre uma
+mensagem caso o valor seja inválido e continue pedindo até que o
+usuário informe um valor válido.
+ */
+
 public class ValidaNotas {
 
 	public static void main(String[] args) {
