634cdcfc4c69b85ae0180c8a953b6e471745a67e
KarenF <karenf@outlook.com>
2020-02-24 17:52:28 -0300

    add maiorNumero

---
+++ src/br/com/java/maiorNumero/MaiorNumero.java
@@ -0,0 +1,28 @@
+package br.com.java.maiorNumero;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Faça um programa que leia 5 números e informe o maior número.
+ */
+public class MaiorNumero {
+
+	public static void main(String[] args) {
+
+		List<Integer> numeros = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Insira um número: ");
+			numeros.add(scanner.nextInt());
+		}
+		
+		numeros.sort((s1, s2) -> Integer.compare(s1, s2));
+		
+		System.out.println("Maior número digitado: " + Collections.max(numeros));
+	}
+
+}
