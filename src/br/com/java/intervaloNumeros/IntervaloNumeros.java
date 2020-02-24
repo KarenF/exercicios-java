be4065be7d36bcd70d64214688e13cef3208cd97
KarenF <karenf@outlook.com>
2020-02-24 18:29:11 -0300

    refatora intervaloNumeros

--- src/br/com/java/intervaloNumeros/IntervaloNumeros.java
+++ src/br/com/java/intervaloNumeros/IntervaloNumeros.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 /*
 Faça um programa que receba dois números inteiros e gere os
 números inteiros que estão no intervalo compreendido por eles.
+
+Altere o programa anterior para mostrar no final a soma dos números
  */
 public class IntervaloNumeros {
 
@@ -15,23 +17,29 @@ public class IntervaloNumeros {
 		int numero1 = scanner.nextInt();
 		System.out.println("Digite o segundo número:");
 		int numero2 = scanner.nextInt();
+		int somaTotal = numero1 + numero2;
 
-		if (numero1 < numero2) {
+		if ((numero1 < numero2) && (numero2 - numero1 > 1)) {
 
 			for (int i = numero1 + 1; i < numero2; i++) {
 
 				System.out.println("Números do intervalo: " + i);
+				somaTotal += i;
 			}
+			System.out.println("Soma total dos números: " + somaTotal);
 
-		} else if (numero2 < numero1) {
+		} else if ((numero2 < numero1) && (numero1 - numero2 > 1)) {
 
 			for (int i = numero2 + 1; i < numero1; i++) {
 
 				System.out.println("Números do intervalo: " + i);
+				somaTotal += i;
 			}
+			System.out.println("Soma total dos números: " + somaTotal);
 		
 		} else {
 			System.out.println("Não há intervalo de números");
+			System.out.println("Soma total dos números: " + somaTotal);
 		}
 	}
 }
