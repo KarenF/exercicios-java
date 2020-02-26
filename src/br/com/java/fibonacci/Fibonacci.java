d7cd4ec71790e8ff22b6cef0114c8f93ce562f30
KarenF <karenf@outlook.com>
2020-02-25 23:46:15 -0300

    refatora fibonacci

--- src/br/com/java/fibonacci/Fibonacci.java
+++ src/br/com/java/fibonacci/Fibonacci.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 A série de Fibonacci é formada pela seqüência
 1,1,2,3,5,8,13,21,34,55,... Faça um programa capaz de gerar a série
 até o n−ésimo termo.
+
+A série de Fibonacci é formada pela seqüência
+0,1,1,2,3,5,8,13,21,34,55,... Faça um programa que gere a série até
+que o valor seja maior que 500.
  */
 public class Fibonacci {
 
@@ -17,6 +21,14 @@ public class Fibonacci {
 
 		for (int i = 0; i <= n; i++)
 			System.out.println("Fibonacci " + i + ": " + Fibonacci.fibo(i));
+
+		System.out.println();
+		
+		int i = 0;
+		do {
+			System.out.println("Fibonacci " + i + ": " + Fibonacci.fibo(i));
+			i++;
+		} while(Fibonacci.fibo(i) <= 610);
 	}
 
 	static long fibo(int n) {
